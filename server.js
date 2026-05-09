430d6ce4a82da4821adaaf7aac1df54f70ded967
mr23raafat-spec <mr23raafat@gmail.com>
2026-05-09 19:47:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -48,21 +48,17 @@ const upload = multer({
 });
 
 // Google Drive Auth with Service Account
-const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: 'service_account',
-    project_id: process.env.GCLOUD_PROJECT,
-    private_key_id: process.env.GCLOUD_PRIVATE_KEY_ID,
-    private_key: process.env.GCLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.GCLOUD_CLIENT_EMAIL,
-    client_id: process.env.GCLOUD_CLIENT_ID,
-    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    token_uri: 'https://oauth2.googleapis.com/token',
-    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_x509_cert_url: process.env.GCLOUD_CLIENT_X509_URL,
-    universe_domain: 'googleapis.com'
-  },
-  scopes: ['https://www.googleapis.com/auth/drive.file']
+// ✅ Google Drive Auth (Service Account - CORRECT)
+const auth = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  ['https://www.googleapis.com/auth/drive']
+);
+
+const drive = google.drive({
+  version: 'v3',
+  auth
 });
 
 const drive = google.drive({ version: 'v3', auth });
